97ced9a1f9b795e5505a38d834efe1fbbec46c25
Marco Tammaro <marcotammaro42@gmail.com>
2021-08-19 11:24:52 +0200

    Added profile view

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forat/firebase_wrappers/auth_wrapper.dart';
 
 class ProfileView extends StatefulWidget {
   @override
@@ -8,8 +9,95 @@ class ProfileView extends StatefulWidget {
 class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).backgroundColor,
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              infoCell(),
+              logoutButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget infoCell() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Text(
+              "Current user",
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Theme.of(context).unselectedWidgetColor.withAlpha(100),
+                  blurRadius: 10,
+                )
+              ],
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Email",
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  AuthWrapper.instance.getCurrentUserEmail(),
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 14),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Username",
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  AuthWrapper.instance.getCurrentUsername(),
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 14),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget logoutButton() {
+    return MaterialButton(
+      onPressed: () => AuthWrapper.instance.logoutUser(),
+      color: Theme.of(context).primaryColor,
+      textColor: Theme.of(context).backgroundColor,
+      child: Text("Log Out"),
+      padding: EdgeInsets.all(12),
+      minWidth: 200,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
